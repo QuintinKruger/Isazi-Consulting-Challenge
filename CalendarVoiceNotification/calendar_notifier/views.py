c5ed10ac62929c2e45336cda7d586abaf98867d1
QuintinKruger <krugerquintin5@gmail.com>
2019-10-03 00:08:57 +0200

    Port error fixed

--- CalendarVoiceNotification/calendar_notifier/views.py
+++ CalendarVoiceNotification/calendar_notifier/views.py
@@ -44,7 +44,7 @@ def get_calendar(request):
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=8001)
+            creds = flow.run_local_server()
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
